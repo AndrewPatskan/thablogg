5d016c2b6d1daa4f2cdd6c8dbe98135e40e75c35
AndrewPatskan <stmyni@gmail.com>
2019-08-24 09:05:19 +0300

    rp3

--- App.js
+++ App.js
@@ -11,6 +11,10 @@ const cookieParser = require('cookie-parser');
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname,'/public/index.html')));
+app.use(express.static(path.join(__dirname,'/build'))) 
+app.get("*",(req,res) => { 
+    res.sendFile(path.join(__dirname + 'build/index.html')) 
+}) 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
